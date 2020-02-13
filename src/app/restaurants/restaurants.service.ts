007f76249d91bd26fad4b897a7f6b5ec02c0a026
Caio César Silva <caiocesarsilva08@gmail.com>
2020-02-12 22:10:52 -0300

    Avaliação e Itens de Menu
    Implementação do componente de itens de Menu e de Avaliações utilizando pipes Async.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 import {Restaurant} from "./restaurant/restaurant.model"
+import {MenuItem} from "../restaurant-detail/menu-item/menu-item.model"
 import {MEAT_API} from "../app.api"
 import {ErrorHandler} from "../app.error-handler"
 
@@ -20,6 +21,18 @@ export class RestaurantsService {
     restaurantsById(id: string): Observable<Restaurant>{
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
         //não queremos um observable de Response, queremos o observable Restaurant
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
     }
