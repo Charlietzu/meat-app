5db9057871d8b6896ade3a093517d02c66bb5f94
Caio César Silva <caiocesarsilva08@gmail.com>
2020-02-20 21:58:46 -0300

    Feedback visual e Input
    Implementação do feeback visual na página do formulário de compra e criação do componente Input dentro da pasta shared (ficarão componentes a serem utilizados por todo o projeto caso necessário). Implementação de Content Projection no componente de Input.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import {ROUTES} from './app.routes'
 
@@ -19,6 +20,7 @@ import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-ca
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 
 @NgModule({
@@ -34,12 +36,14 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
