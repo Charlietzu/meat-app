1153e1be4a5befe5663c8ef9eb72f2eddca8e3dd
Caio César Silva <caiocesarsilva08@gmail.com>
2020-03-01 17:59:45 -0300

    CoreModule e SharedModule
    Implementação de uma função no SharedModule que irá chamar os providers condicionalmente, tornando o CoreModule obsoleto, logo, apaguei o CoreModule.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
-import {NgModule, Input} from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import {InputComponent} from './input/input.component';
 import {RadioComponent} from './radio/radio.component';
 import {RatingComponent} from './rating/rating.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { OrderService } from "../order/order.service";
 
 @NgModule({
     declarations:[
@@ -26,4 +29,15 @@ import {CommonModule} from '@angular/common';
     ]
 })
 
-export class SharedModule{}
+export class SharedModule{
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers:[
+                ShoppingCartService,
+                RestaurantsService,
+                OrderService
+            ]
+        }
+    }
+}
